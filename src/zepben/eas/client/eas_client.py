4e770cca61994b1207a7c85fef7f808039415c87
Marcus Koh <marcuskoh29@gmail.com>
2022-02-10 16:56:22 +1100

    Warn when non-ok response from upload_study
    Signed-off-by: Marcus Koh <marcuskoh29@gmail.com>

--- src/zepben/eas/client/eas_client.py
+++ src/zepben/eas/client/eas_client.py
@@ -118,7 +118,7 @@ class EasClient:
         with warnings.catch_warnings():
             if self.__verify_certificate is False:
                 warnings.filterwarnings("ignore", category=InsecureRequestWarning)
-            return requests.post(
+            response = requests.post(
                 construct_url(
                     protocol=self.__protocol,
                     host=self.__host,
@@ -162,3 +162,7 @@ class EasClient:
                 },
                 verify=self.__verify_certificate
             )
+            if not response.ok:
+                warnings.warn(f"Attempted to upload study; "
+                              f"Evolve App Server responded with {response.status_code}: {response.text}")
+            return response
