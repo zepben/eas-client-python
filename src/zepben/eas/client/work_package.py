68f95931efa6d809093aec2b74b0f597674c7c6b
Roberto Marquez <roberto.marquez@zepben.com>
2023-08-16 19:05:27 +1000

    Updated code to support latest changes to the hosting capacity work package request.
    Signed-off-by: Roberto Marquez <roberto.marquez@zepben.com>

--- src/zepben/eas/client/work_package.py
+++ src/zepben/eas/client/work_package.py
@@ -5,9 +5,69 @@
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 from dataclasses import dataclass
-from typing import List
+from enum import Enum
+from typing import List, Optional
 
-__all__ = ["WorkPackageConfig"]
+__all__ = ["WorkPackageConfig",
+           "SwitchClass",
+           "SwitchMeterPlacementConfig",
+           "ModelConfig",
+           "SolveMode",
+           "SolveConfig",
+           "ResultsDetailLevel"]
+
+
+class SwitchClass(Enum):
+    BREAKER = "BREAKER",
+    DISCONNECTOR = "DISCONNECTOR",
+    FUSE = "FUSE",
+    JUMPER = "JUMPER",
+    LOAD_BREAK_SWITCH = "LOAD_BREAK_SWITCH",
+    RECLOSER = "RECLOSER"
+
+
+@dataclass
+class SwitchMeterPlacementConfig:
+    meterSwitchClass: Optional[SwitchClass] = None
+    namePattern: Optional[str] = None
+
+
+@dataclass
+class ModelConfig:
+    vmPu: Optional[float] = None
+    vMinPu: Optional[float] = None
+    vMaxPu: Optional[float] = None
+    loadModel: Optional[int] = None
+    collapseSWER: Optional[bool] = None
+    meterAtHVSource: Optional[bool] = None
+    metersAtDistTransformers: Optional[bool] = None
+    switchMeterPlacementConfigs: Optional[List[SwitchMeterPlacementConfig]] = None
+
+
+class SolveMode(Enum):
+    YEARLY = "YEARLY"
+    DAILY = "DAILY"
+
+
+@dataclass
+class SolveConfig:
+    normVMinPu: Optional[float] = None
+    normVMaxPu: Optional[float] = None
+    emergVMinPu: Optional[float] = None
+    emergVMaxPu: Optional[float] = None
+    baseFrequency: Optional[int] = None
+    voltageBases: Optional[List[float]] = None
+    maxIter: Optional[int] = None
+    maxControlIter: Optional[int] = None
+    mode: Optional[SolveMode] = None
+    stepSizeMinutes: Optional[float] = None
+
+
+class ResultsDetailLevel(Enum):
+    STANDARD = "STANDARD"
+    BASIC = "BASIC"
+    EXTENDED = "EXTENDED"
+    RAW = "RAW"
 
 
 @dataclass
@@ -16,3 +76,7 @@ class WorkPackageConfig:
     feeders: List[str]
     years: List[int]
     scenarios: List[str]
+    modelConfig: Optional[ModelConfig] = None
+    solveConfig: Optional[SolveConfig] = None
+    resultsDetailLevel: Optional[ResultsDetailLevel] = None
+    qualityAssuranceProcessing: Optional[bool] = None
