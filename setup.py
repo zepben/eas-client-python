d4d5317b89f533f8c863f54be082a736ac323a1c
Kurt Greaves <kurt.greaves@zepben.com>
2023-11-20 11:34:33 +1100

    Use fixed zepben.auth for latest EAS version.
    Signed-off-by: Kurt Greaves <kurt.greaves@zepben.com>

--- setup.py
+++ setup.py
@@ -26,8 +26,8 @@ setup(
         "geojson==2.5.0",
         "requests==2.26.0",
         "urllib3==1.26.6",
-        "zepben.auth==0.10.0",
-        "aiohttp[speedups]==3.8.1",
+        "zepben.auth==0.12.0b1",
+        "aiohttp[speedups]==3.9.0",
     ],
     extras_require={
         "test": test_deps,
