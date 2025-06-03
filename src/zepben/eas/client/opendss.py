b79bbb55373d20dca0be7ee0e9a434bb6c3c2c05
clydeu <clyde.uyenghua@zepben.com>
2025-06-03 21:55:52 +1000

    pagedOpenDssModels
    Signed-off-by: clydeu <clyde.uyenghua@zepben.com>

--- src/zepben/eas/client/opendss.py
+++ src/zepben/eas/client/opendss.py
@@ -4,11 +4,16 @@
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
 from dataclasses import dataclass
-from typing import Union, Optional
+from enum import Enum
+from typing import Union, Optional, List
 from zepben.eas.client.work_package import GeneratorConfig, TimePeriod, FixedTime
 
 __all__ = [
-    "OpenDssConfig"
+    "OpenDssConfig",
+    "OpenDssModelState",
+    "GetOpenDssModelsFilterInput",
+    "Order",
+    "GetOpenDssModelsSortCriteriaInput"
 ]
 
 @dataclass
@@ -21,3 +26,28 @@ class OpenDssConfig:
     generator_config: Optional[GeneratorConfig] = None
     model_name: Optional[str] = None
     is_public: Optional[bool] = None
+
+class OpenDssModelState(Enum):
+    COULD_NOT_START = "COULD_NOT_START"
+    CREATION = "CREATION"
+    COMPLETED = "COMPLETED"
+    FAILED = "FAILED"
+
+@dataclass
+class GetOpenDssModelsFilterInput:
+    """ A data class representing the filter to apply to the opendss export run paginated query """
+    name: Optional[str] = None
+    is_public: Optional[int] = None
+    state: Optional[List[OpenDssModelState]] = None
+
+class Order(Enum):
+    ASC = "ASC"
+    DESC = "DESC"
+
+@dataclass
+class GetOpenDssModelsSortCriteriaInput:
+    """ A data class representing the sort criteria to apply to the opendss export run paginated query """
+    name: Optional[Order] = None
+    created_at: Optional[Order] = None
+    state: Optional[Order] = None
+    is_public: Optional[Order] = None
