b60b33bf2a7c58d1c0b80df93efa6bc3cfef0aa5
Max Chesterfield <max.chesterfield@zepben.com>
2025-08-20 16:08:03 +1000

    update build system
    Signed-off-by: Max Chesterfield <max.chesterfield@zepben.com>

--- setup.py
+++ setup.py
@@ -4,32 +4,6 @@
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-from setuptools import setup, find_namespace_packages
+import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-test_deps = ["pytest", "pytest-cov", "pytest-httpserver==1.0.8", "trustme==0.9.0"]
-setup(
-    name="zepben.eas",
-    version="0.23.0b1",
-    description="Python SDK for interacting with the Evolve App Server",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/zepben/eas-python-client",
-    author="Ramon Bouckaert",
-    author_email="ramon.bouckaert@zepben.com",
-    package_dir={"": "src"},
-    python_requires='>=3.9',
-    packages=find_namespace_packages(where="src"),
-    install_requires=[
-        "geojson==2.5.0",
-        "requests<3.0.0,>=2.26.0",
-        "urllib3==1.26.6",
-        "zepben.auth==0.12.1",
-        "aiohttp[speedups]==3.9.0",
-    ],
-    extras_require={
-        "test": test_deps,
-    }
-)
+setuptools.setup()
