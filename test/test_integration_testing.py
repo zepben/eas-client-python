e772d0af726b211d9bc07f6df5a2a784623e791e
Max Chesterfield <max.chesterfield@zepben.com>
2026-03-31 14:32:17 +1100

    code gen via ast ftw
    Signed-off-by: Max Chesterfield <max.chesterfield@zepben.com>

--- test/test_integration_testing.py
+++ test/test_integration_testing.py
@@ -3,14 +3,17 @@
 #  This Source Code Form is subject to the terms of the Mozilla Public
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+import ast
 import asyncio
-from typing import TypeVar
+from typing import TypeVar, TYPE_CHECKING
 
 import pytest
 
 from zepben.eas import EasClient, OpenDssModelInput, OpenDssModelGenerationSpecInput, OpenDssModelOptionsInput, \
-    OpenDssModulesConfigInput, OpenDssCommonConfigInput, Query, IngestionRunFields, GraphQLQuery, IngestionJobFields
+    OpenDssModulesConfigInput, OpenDssCommonConfigInput, Query, IngestionRunFields, IngestionJobFields
 
+if TYPE_CHECKING:
+    from zepben.eas import GraphQLQuery
 
 @pytest.mark.skip("Local testing if you really want it...")
 def test_can_connect_to_local_eas_non_async():
@@ -64,11 +67,70 @@ T = TypeVar("T")
 R = TypeVar("R")
 
 
-def my_query(query: GraphQLQuery[T, R], field: R, *additional_fields: R) -> T:
+def my_query(query: "GraphQLQuery[T, R]", field: R, *additional_fields: R) -> T:
     return query.fields(field, *additional_fields)
 
+ast.ClassDef(
+    "MyClass",
+    [],
+    [],
+    [
+        ast.FunctionDef(
+            "MyFunction",
+            ast.arguments(
+                posonlyargs=[],
+                args=[],
+                vararg=None,
+                kwonlyargs=[],
+                kw_defaults=[],
+                defaults=[],
+            ),
+            [],
+            [],
+            None,
+        )
+    ],
+    [],
+)
 
 def test_do_things():
+    from zepben.eas.lib.generated_graphql_client import custom_queries
+    with open(custom_queries.__file__) as f:
+        orig_ast = ast.parse(
+            f.read(),
+        )
+    n = None
+    extra_imports = []
+
+    for i, b in enumerate(orig_ast.body):
+        if isinstance(b, ast.ClassDef):
+            if n is None:
+                n = i
+            for func in b.body:
+                if isinstance(func, ast.FunctionDef):
+                    func.body = [ast.Pass()]
+                    extra_imports.append(func.returns.id.replace("Fields", "GraphQLField"))
+                    func.returns = ast.Name(f'\"GraphQLQuery[{func.returns.id}, {func.returns.id.replace("Fields", "GraphQLField")}]\"')
+
+    orig_ast.body.insert(n, ast.parse(
+            """
+class GraphQLQuery(Generic[TGraphQLQueryField, TGraphQLField]):
+    def fields(self, *fields: TGraphQLField):  ...
+        """
+    ).body[0])
+    orig_ast.body.insert(n, ast.parse(
+            'TGraphQLField = TypeVar("TGraphQLField")'
+    ).body[0])
+    orig_ast.body.insert(n, ast.parse(
+        'TGraphQLQueryField = TypeVar("TGraphQLQueryField")'
+    ).body[0])
+
+    orig_ast.body.insert(n, ast.parse("from typing import Generic, TypeVar").body[0])
+    orig_ast.body.insert(n, ast.parse(f"from zepben.eas import {', '.join(extra_imports)}").body[0])
+
+    with open(custom_queries.__file__ + 'i', 'w') as f:
+        f.write(ast.unparse(orig_ast))
+
     my_query(Query.list_ingestor_runs(filter_=None, sort=None), IngestionRunFields.completed_at, IngestionRunFields.status)
     my_query(Query.list_ingestor_runs(filter_=None, sort=None), IngestionJobFields.application, 1)
-    my_query(Query.list_ingestor_runs(filter_=None, sort=None))
+    # my_query(Query.list_ingestor_runs(filter_=None, sort=None))
